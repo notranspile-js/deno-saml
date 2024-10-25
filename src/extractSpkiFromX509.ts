1a61cbdd28f1b0d27c32ac195541669e1c2bab12
alex <alex@staticlibs.net>
2024-10-25 21:41:58 +0100

    fix spki extraction from x509 cert

--- src/extractSpkiFromX509.ts
+++ src/extractSpkiFromX509.ts
@@ -28,53 +28,86 @@ function checkOctet(der: Uint8Array, idx: number, expected: number) {
   }
 }
 
+function parseLength(bytes: Uint8Array, offset = 0) {
+  // Get the initial length byte
+  const firstByte = bytes[offset];
+  
+  // Short form length
+  if (firstByte < 0x80) {
+    return {
+      length: firstByte,
+      lengthBytes: 1,
+      totalLength: firstByte + 1 // Includes the first length byte itself
+    };
+  }
+  
+  // Long form length
+  const numLengthBytes = firstByte & 0x7F; // Mask off the MSB to get the count of length bytes
+  let length = 0;
+  
+  for (let i = 1; i <= numLengthBytes; i++) {
+    length = (length << 8) | bytes[offset + i]; // Combine bytes to form the length
+  }
+  
+  return {
+    length,
+    lengthBytes: numLengthBytes + 1, // Includes the first length byte itself
+    totalLength: length + numLengthBytes + 1
+  };
+}
+
 // https://en.wikipedia.org/wiki/X.509#Structure_of_a_certificate
 // openssl asn1parse -in cert.der -inform der -i
 function extractPubKey(der: Uint8Array) {
+  let pos = 0;
   // SEQUENCE
   checkOctet(der, 0, 0x30);
-  checkOctet(der, 1, 0x82);
+  pos += 1;
+  const seq1Len = parseLength(der, pos);
+  pos += seq1Len.lengthBytes;
   // SEQUENCE
   checkOctet(der, 4, 0x30);
-  checkOctet(der, 5, 0x82);
+  pos += 1;
+  const seq2Len = parseLength(der, pos);
+  pos += seq2Len.lengthBytes;
   // cont [ 0 ]
+  checkOctet(der, pos, 0xa0);
+  pos += 1;
+  const versionLen = parseLength(der, pos);
+  pos += versionLen.totalLength;
   // INTEGER
-  const versionStart = 8;
-  checkOctet(der, versionStart, 0xa0);
-  const versionLen = 2 + der[versionStart + 1];
-  // INTEGER
-  const serialNumStart = versionStart + versionLen;
-  checkOctet(der, serialNumStart, 0x02);
-  const serialNumLen = 2 + der[serialNumStart + 1];
+  checkOctet(der, pos, 0x02);
+  pos += 1;
+  const serialNumLen = parseLength(der, pos);
+  pos += serialNumLen.totalLength;
   // SEQUENCE
-  const sigAlgIdStart = serialNumStart + serialNumLen;
+  const sigAlgIdStart = pos;
   checkOctet(der, sigAlgIdStart, 0x30);
-  const sigAlgIdLen = 2 + der[sigAlgIdStart + 1];
+  pos += 1;
+  const sigAlgIdLen = parseLength(der, pos);
+  pos += sigAlgIdLen.totalLength;
   // SEQUENCE
-  const issuerNameStart = sigAlgIdStart + sigAlgIdLen;
-  checkOctet(der, issuerNameStart, 0x30);
-  const issuerNameLen = 2 + der[issuerNameStart + 1];
+  checkOctet(der, pos, 0x30);
+  pos += 1;
+  const issuerNameLen = parseLength(der, pos);
+  pos += issuerNameLen.totalLength;
   // SEQUENCE
-  const validityPeriodStart = issuerNameStart + issuerNameLen;
-  checkOctet(der, validityPeriodStart, 0x30);
-  const validityPeriodLen = 2 + der[validityPeriodStart + 1];
+  checkOctet(der, pos, 0x30);
+  pos += 1;
+  const validityPeriodLen = parseLength(der, pos);
+  pos += validityPeriodLen.totalLength;
   // SEQUENCE
-  const subjectNameStart = validityPeriodStart + validityPeriodLen;
-  checkOctet(der, subjectNameStart, 0x30);
-  let subjectNameLen = -1;
-  if (0x81 == der[subjectNameStart + 1]) {
-    subjectNameLen = 3 + der[subjectNameStart + 2];
-  } else {
-    subjectNameLen = 2 + der[subjectNameStart + 1];
-  }
+  checkOctet(der, pos, 0x30);
+  pos += 1;
+  const subjectNameLen = parseLength(der, pos);
+  pos += subjectNameLen.totalLength;
   // SEQUENCE
-  const publicKeyStart = subjectNameStart + subjectNameLen;
-  checkOctet(der, publicKeyStart, 0x30);
-  checkOctet(der, publicKeyStart + 1, 0x82);
-  const publicKeyLen = 4 + (der[publicKeyStart + 2] * 256) +
-    der[publicKeyStart + 3];
-
-  const view = new Uint8Array(der.buffer, publicKeyStart, publicKeyLen);
+  const publicKeyStart = pos;
+  checkOctet(der, pos, 0x30);
+  pos += 1;
+  const publicKeyLen = parseLength(der, pos);
+  const publicKeyFullLenNum = publicKeyLen.totalLength + 1;
+  const view = new Uint8Array(der.buffer, publicKeyStart, publicKeyFullLenNum);
   return new Uint8Array(view);
 }
 
